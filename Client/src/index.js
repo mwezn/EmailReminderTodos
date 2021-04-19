e245c91f8c95864f4167a02d221bbc187fec2b83
mwezn <m.w.g.nelson@gmail.com>
2021-04-19 08:58:39 +0100

    Polyfill support for Internet explorer

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,4 +1,7 @@
 
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import  {App } from './App.js'
